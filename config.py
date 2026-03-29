257e7c0914724ea4b670519b7c83323cd383f012
svsvsvs111 <72559207+svsvsvs111@users.noreply.github.com>
2026-03-29 23:16:47 +0300

    Update config.py
    import os
    from dotenv import load_dotenv
    
    load_dotenv()
    
    BOT_TOKEN = os.getenv("BOT_TOKEN")
    
    BASE_URL = os.getenv("LIKECARD_BASE_URL")
    
    EMAIL = os.getenv("LIKECARD_EMAIL")
    DEVICE_ID = os.getenv("LIKECARD_DEVICE_ID")
    SECURITY_CODE = os.getenv("LIKECARD_SECURITY_CODE")
    
    CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", 15))

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 BASE_URL = os.getenv("LIKECARD_BASE_URL")
-API_KEY = os.getenv("LIKECARD_API_KEY")
-SECRET = os.getenv("LIKECARD_SECRET")
+
+EMAIL = os.getenv("LIKECARD_EMAIL")
+DEVICE_ID = os.getenv("LIKECARD_DEVICE_ID")
+SECURITY_CODE = os.getenv("LIKECARD_SECURITY_CODE")
 
 CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL", 15))
-ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
